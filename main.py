e70fb5d4cd9b0e8f6d51e0fe17757c9d8fad5f52
valisoa01 <valisoatolotriniaina@gmail.com>
2025-07-24 06:40:34 +0000

    examen-Api

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
-@app.get("/")
+@app.get("/hello")
 async def root():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
+@app.get("/welcome/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"message": f"Welcome {name}"}
+
+class Student(BaseModel):
+    reference: str
+    name: str
+    age: int
+    first_name: str
+
+users_db: list[Student] = []
+
+@app.post("/students")
+async def create_student(student: Student):
+    users_db.append(student)
+    return {"message": f"Added {student.reference}"}
+
+@app.get("/students")
+async def get_students():
+    return users_db
+
+@app.put("/students/{reference}")
+async def update_student(reference: str, student: Student):
+    for i, user in enumerate(users_db):
+        if user.reference == reference:
+            users_db[i] = student
+            return {"message": f"Updated student with reference {reference}"}
+    return {"error": "Student not found"}
